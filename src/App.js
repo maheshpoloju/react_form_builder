44c3d60e7e784072896dffa5854f8135c0c11646
maheshpoloju <polojumahesh3@gmail.com>
2022-11-08 18:06:18 +0530

    message

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import { FormBuilder } from "cb-react-forms";
+import { useState } from "react";
+import Modal from "react-modal";
+import "./App.css";
+
+const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+  },
+};
 
 function App() {
+  const [formData, setFormData] = useState([]);
+  const [showModal, setShowModal] = useState(false);
+
+  function closeModal() {
+    setShowModal(false);
+  }
+  const handleFormData = (data) => {
+    var obj = JSON.parse(data);
+
+    setFormData(obj);
+    setShowModal((prev) => !prev);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      {!showModal ? (
+        <FormBuilder onSubmit={(data) => handleFormData(data)} />
+      ) : (
+        <Modal
+          isOpen={showModal}
+          onRequestClose={closeModal}
+          style={customStyles}
+          contentLabel="Example Modal"
         >
-          Learn React
-        </a>
-      </header>
+          <div className="form-data">
+            {formData &&
+              formData.map((each) => {
+                const { label, options } = each;
+                const { blocks } = label;
+                const labelText = blocks[0].text;
+
+                return (
+                  <div>
+                    <h1>Label: {labelText}</h1>
+                    {options.map((each) => (
+                      <h1>Option: {each.value}</h1>
+                    ))}
+                  </div>
+                );
+              })}
+          </div>
+        </Modal>
+      )}
     </div>
   );
 }
